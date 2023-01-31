af93387b0c240073a69ca15f97f73915482d9e14
KalinYordanov <kalinyordanov920815@gmail.com>
2023-01-31 18:44:11 +0200

    Create Car.java

---
+++ DefiningClassesExercises/RawData/Car.java
@@ -0,0 +1,35 @@
+package RawData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Car {
+    private String model;
+    private Engine engine;
+    private Cargo cargo;
+    private List<Tire> tires;
+
+    public Car(String model, Engine engine, Cargo cargo, List<Tire> tires) {
+        this.model = model;
+        this.engine = engine;
+        this.cargo = cargo;
+        this.tires = new ArrayList<>();
+        this.tires.addAll(tires);
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+
+    public List<Tire> getTires() {
+        return tires;
+    }
+}
