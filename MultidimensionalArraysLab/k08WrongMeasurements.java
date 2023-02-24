612360818c43b1aa192531a56a20311686d81a13
KalinYordanov <kalinyordanov920815@gmail.com>
2023-02-24 13:50:44 +0200

    Delete k08WrongMeasurements.java

--- MultidimensionalArraysLab/k08WrongMeasurements.java
+++
@@ -1,8 +0,0 @@
-package MultidimensionalArraysLab;
-import java.util.Scanner;
-
-public class k08WrongMeasurements {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    }
-}
