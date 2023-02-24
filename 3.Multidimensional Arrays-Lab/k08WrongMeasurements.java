02ee4d66cef742a4b57783398caa7b17ad6dd115
KalinYordanov <kalinyordanov920815@gmail.com>
2023-02-24 13:50:10 +0200

    Create k08WrongMeasurements.java

---
+++ 3.Multidimensional Arrays-Lab/k08WrongMeasurements.java
@@ -0,0 +1,8 @@
+package MultidimensionalArraysLab;
+import java.util.Scanner;
+
+public class k08WrongMeasurements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
