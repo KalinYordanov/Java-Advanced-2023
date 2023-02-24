7b43d2df67f686400e5a8c3b30d745a38dbfa67d
KalinYordanov <kalinyordanov920815@gmail.com>
2023-02-24 17:47:53 +0200

    Create Cat.java

---
+++ 12.DefiningClassesExercises/CatLady/Cat.java
@@ -0,0 +1,12 @@
+package CatLady;
+
+public class Cat {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
