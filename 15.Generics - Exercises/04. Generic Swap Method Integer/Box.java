0072a71b5e9aa6fe1c98abcb45d801d73c22c681
KalinYordanov <kalinyordanov920815@gmail.com>
2023-02-27 00:26:53 +0200

    Create Box.java

---
+++ 15.Generics - Exercises/04. Generic Swap Method Integer/Box.java
@@ -0,0 +1,31 @@
+package box;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T> {
+    private List<T> values;
+
+    public Box() {
+        this.values = new ArrayList<>();
+    }
+
+    public void add(T element) {
+        values.add(element);
+    }
+
+    public void swap(int firstIndex, int secondIndex) {
+        T temp = values.get(firstIndex);
+        values.set(firstIndex, values.get(secondIndex));
+        values.set(secondIndex, temp);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (T value : values) {
+            sb.append(String.format("%s: %s%n", value.getClass().getName(), value.toString()));
+        }
+        return sb.toString();
+    }
+}
