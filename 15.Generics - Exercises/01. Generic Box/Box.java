3f5be5ce50af23ab80b11ba7f1f466af13a73dfb
KalinYordanov <kalinyordanov920815@gmail.com>
2023-02-26 23:39:44 +0200

    Create Box.java

---
+++ 15.Generics - Exercises/01. Generic Box/Box.java
@@ -0,0 +1,25 @@
+package box;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T> {
+    private List<T> values;
+
+    public Box() {
+        this.values = new ArrayList<>();
+    }
+
+    public void add(T element) {
+        values.add(element);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (T value : values) {
+            sb.append(String.format("%s: %s%n", value.getClass().getName(), value.toString()));
+        }
+        return sb.toString();
+    }
+}
