9abc6aa5ad3b3f9a6e073314285e11c4b1b94862
KalinYordanov <kalinyordanov920815@gmail.com>
2023-02-27 14:40:07 +0200

    Create Main.java

---
+++ 15.Generics - Exercises/06. Generic Count Method Double/Main.java
@@ -0,0 +1,20 @@
+package box;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        Box<Double> box = new Box<>();
+
+        for (int i = 0; i < n; i++) {
+            Double element = Double.parseDouble(scanner.nextLine());
+            box.add(element);
+        }
+
+        Double element = Double.parseDouble(scanner.nextLine());
+        System.out.println(box.countOfGreatItems(element));
+    }
+}
